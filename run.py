19e6bc1d9755e8450d2925e7be3ef4c512ba65fe
Pawel Wodyk <pawlosik@yahoo.com>
2019-05-27 22:54:33 +0000

    added basic chat functionality with time stamps

--- run.py
+++ run.py
@@ -1,26 +1,37 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
+def add_messages(username, message):
+    """ adds the user and the message to the list """
+    timestamp = datetime.now().strftime('%H:%M:%S')
+    messages.append("({}) {}: {}".format(timestamp, username, message))
+    
+
+def get_all_messages():
+    """ return all messages, formated on seperate lines"""
+    return "<br>".join(messages)
 
 @app.route("/")
 def index():
-    """
-    Main page with instructions
-    """
-
+    """ Main page with instructions """
     return "To send a message use: /USERNAME/MESSAGE"
 
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """ Display chat messages """
+    return "<h1>Welcome, {0}</h1><hr> {1}".format(username, get_all_messages())
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """ Create a new message and redirect back to the chat page """
+    add_messages(username, message)
+    return redirect("/" + username)
     
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=os.getenv('PORT', 5500), debug=True)
