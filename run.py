4fc5b47204d851191e32fdbc30150c40aedeb126
Pawel Wodyk <pawlosik@yahoo.com>
2019-05-27 12:15:22 +0000

    added basic routing

--- run.py
+++ run.py
@@ -3,9 +3,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    """
+    Main page with instructions
+    """
+
+    return "To send a message use: /USERNAME/MESSAGE"
+
+
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
     
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', 8080), debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'), port=os.getenv('PORT', 5500), debug=True)
